3b06da1a62c57ab8c932fd3fcc400972a8900480
unknown <akxltmzk@gmail.com>
2020-03-24 18:03:11 +0900

    add python generator

--- python_scrapper/app.py
+++ python_scrapper/app.py
@@ -1,11 +1,28 @@
-from instagram import gallery_url
+from instagram import gallery_url,close_browser
 import requests, json
 import sys 
 
-username = sys.argv[1]
-gallery_url = gallery_url(username)
-print(gallery_url)
-sys.stdout.flush()
+gallery_url_array = ''
+
+def get_user_name():
+  username = sys.argv[1]
+  global gallery_url_array
+  gallery_url_array = gallery_url(username)
+
+def send_data_to_scrapper():
+  global gallery_url_array
+  print(gallery_url_array)
+  sys.stdout.flush()
+
+def generator():
+  yield get_user_name()
+  yield send_data_to_scrapper()
+  yield close_browser()
+
+g = generator()
+next(g)
+next(g)
+next(g)
 
 
 # gallery_url = gallery_url('jjong.h')
